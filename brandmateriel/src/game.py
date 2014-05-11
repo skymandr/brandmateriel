cffdd57b678dd73c9c3f4615e3edede9e0cae7c5
Andreas Skyman <skymandr@fripost.org>
2014-05-11 19:43:27 +0200

    started working on the game...

--- brandmateriel/src/game.py
+++ brandmateriel/src/game.py
@@ -12,5 +12,38 @@ KEYBOARD = {l.K_UP: 'up', l.K_k: 'up', l.K_w: 'up',
 
 
 class Game(object):
-    def __init__(self):
+    """
+    Here be docstring.
+    """
+
+    X = U = 0
+    Y = V = 1
+    Z = W = 2
+
+    def __init__(self, config):
+        self._config = config
+
+        self._populate_world()
+
+    @property
+    def _position(self):
+        return None
+
+    @property
+    def _view(self):
+        return self._config["view"]
+
+    @property
+    def map_view(self):
+        position = np.round(self._position).astype(np.int)
+        view = self._view
+        X, Y = np.mgrid[position[self.Y] - view[self.Y] / 2 - 1:
+                        position[self.Y] + view[self.Y] / 2 + 1,
+                        position[self.X] - view[self.X] / 2 - 1:
+                        position[self.X] + view[self.X] / 2 + 1]
+        X, Y = X % self.world.shape[self.X], Y % self.world.shape[self.Y]
+
+        return self.world.positions_array[(Y, X), :]
+
+    def _populate_world(self):
         pass
