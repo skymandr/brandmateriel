2750886bdf81264604d06e0d8ec6de6bb193934a
Andreas Skyman <skymandr@fripost.org>
2014-05-14 10:22:03 +0200

    changed units to 2x2 tiles

--- brandmateriel/src/engine/mobs.py
+++ brandmateriel/src/engine/mobs.py
@@ -141,7 +141,7 @@ class Player(Movable):
     Player object has health, fuel etc.
     """
 
-    def __init__(self, model, inertia=1.0, gravity=10.0, friction=1.0,
+    def __init__(self, model, inertia=1.0, gravity=5.0, friction=0.5,
                  position=np.array([0.0, 0.0, 0.0]),
                  velocity=np.array([0.0, 0.0, 0.0]),
                  acceleration=np.array([0.0, 0.0, 0.0]),
