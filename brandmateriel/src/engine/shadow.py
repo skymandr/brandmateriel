bd7ed9cae128706cccbf676eee75b91526956aaf
Andreas Skyman <skymandr@fripost.org>
2014-05-21 23:05:09 +0200

    a simplification

--- brandmateriel/src/engine/shadow.py
+++ brandmateriel/src/engine/shadow.py
@@ -5,17 +5,10 @@ Y = V = 1
 Z = W = 2
 
 
-def get_shadows(patches, positions, world):
+def get_shadows(patches, world):
     patches[..., Z] = world.patch_positions[patches[..., X].astype(np.int),
                                               patches[..., Y].astype(np.int),
-                                              Z]
-    patches[..., Z] = np.where(patches[..., Z] < 0, 0, patches[..., Z])
+                                              Z] + 0.01
+    patches[..., Z] = np.where(patches[..., Z] < 0, 0, patches[..., Z]) + 0.01
 
-    positions[..., Z] = world.map_positions[positions[..., X].astype(np.int),
-                                            positions[..., Y].astype(np.int),
-                                            Z]
-    positions[..., Z] = np.where(positions[..., Z] < 0, 0, positions[..., Z])
-
-    print positions[..., Z]
-
-    return patches, positions
+    return patches, patches.mean(-2)
