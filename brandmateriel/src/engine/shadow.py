f3a6fabafbce0506546a3f94ad09e3e80f835bc3
Andreas Skyman <skymandr@fripost.org>
2014-05-21 23:43:41 +0200

    made shadow debug-optional

--- brandmateriel/src/engine/shadow.py
+++ brandmateriel/src/engine/shadow.py
@@ -6,9 +6,40 @@ Z = W = 2
 
 
 def get_shadows(patches, world):
-    patches[..., Z] = world.patch_positions[patches[..., X].astype(np.int),
-                                              patches[..., Y].astype(np.int),
-                                              Z] + 0.01
-    patches[..., Z] = np.where(patches[..., Z] < 0, 0, patches[..., Z]) + 0.01
+    """
+    Function for getting shadow of object on ground.
 
-    return patches, patches.mean(-2)
+    The patches Z assigment could benefit from tweaking (weighted mean of
+    map tile heights feasible?), but more than that, drawing the thing
+    is really very difficult, since maptiles drawn after the shadowpatches,
+    can be drawn over the shadow...
+
+    The shadows could also benefit from having their normals defined, so that
+    they can be culled under extreme circumstances. This would be easy.
+    Getting the draworder to work is hard...
+    """
+
+    positions = patches.mean(-2)
+
+    positions[..., Z] = patches[..., Z].min()
+
+    positions[..., Z] = np.where(positions[..., Z] < 0, 0, positions[..., Z])
+
+    patches[..., Z] = world.patch_positions[patches[..., X].astype(np.int),
+                                            patches[..., Y].astype(np.int),
+                                            Z]
+    patches[..., Z] = np.where(patches[..., Z] < 0, 0, patches[..., Z])
+
+    """
+    positions = patches.mean(-2)
+
+    positions[..., Z] = world.patches[positions[..., X].astype(np.int),
+                                      positions[..., Y].astype(np.int),
+                                      :, Z].max(-1)
+
+    positions[..., Z] = np.where(positions[..., Z] < 0, 0, positions[..., Z])
+
+    positions[..., Y] = patches[..., Y].min()
+    """
+
+    return patches, positions
--- brandmateriel/src/game.py
+++ brandmateriel/src/game.py
@@ -306,15 +306,19 @@ class Game(object):
         player_colours = self.shader.apply_lighting(player_positions,
                                                     player_normals,
                                                     player_colours)
-        """
-        player_shadow, player_shadow_positions = e.shadow.get_shadows(
-            self.player.model.patches.copy(), player_positions.copy(),
-            self.world)
-        player_shadow, shadow_depths = self.camera.get_screen_coordinates(
-            player_shadow)
-        player_shadow_colours = np.array([[0, 0, 0, 1]]) * np.ones(
-            (player_colours.shape[0], 1))
-        """
+
+        shadow = False
+        if shadow:
+            player_shadow, player_shadow_positions = e.shadow.get_shadows(
+                self.player.model.patches.copy(), self.world)
+            player_shadow, shadow_depths = self.camera.get_screen_coordinates(
+                player_shadow)
+            player_shadow_colours = np.array([[0, 0, 0, 1]]) * np.ones(
+                (player_colours.shape[0], 1))
+        else:
+            player_shadow_positions = np.empty((0, 3))
+            player_shadow = np.empty((0, 4, 2))
+            player_shadow_colours = np.empty((0, 4))
 
         # Handle particles:
         if self.player.fire and (self.shots.number == 0 or
@@ -395,14 +399,14 @@ class Game(object):
 
         # aggregate object data:
         positions = np.r_[map_positions, player_positions, shots_positions,
-                          exhaust_positions, ]  # player_shadow_positions]
+                          exhaust_positions, player_shadow_positions]
         patches = list(map_patches[:])
         patches.extend(list(player_patches[:]))
         patches.extend(list(shots_patches[:]))
         patches.extend(list(exhaust_patches[:]))
-        # patches.extend(list(player_shadow[:]))
+        patches.extend(list(player_shadow[:]))
         colours = np.r_[map_colours, player_colours, shots_colours,
-                        exhaust_colours, ]  # player_shadow_colours]
+                        exhaust_colours, player_shadow_colours]
 
         # sort patches:
         order = np.argsort(-((positions - self.camera.position) ** 2).mean(-1))
